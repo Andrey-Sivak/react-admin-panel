d7620030d2490f21982c6befa870975771da3be7
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-08-27 01:48:13 +0300

    use pages routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {useState} from 'react';
 import {ColorModeContext, useMode} from "./theme";
 import Sidebar from "./sceens/global/Sidebar";
 import {CssBaseline, ThemeProvider} from "@mui/material";
+import {Routes, Route} from 'react-router-dom';
+import Team from "./sceens/team";
 import Topbar from "./sceens/global/Topbar";
+import Dashboard from "./sceens/dashboard";
+import Contacts from "./sceens/contacts";
+import Invoices from "./sceens/invoices";
+import Form from "./sceens/form";
+import Calendar from "./sceens/calendar";
+import FAQ from "./sceens/FAQ";
+import Bar from "./sceens/bar";
+import Pie from "./sceens/pie";
+import Line from "./sceens/line";
+import Geography from "./sceens/geography";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -14,7 +27,20 @@ function App() {
                 <div className="app">
                     <Sidebar isSidebar={isSidebar} />
                     <main className="content">
-                        <Topbar />
+                        <Topbar setIsSidebar={setIsSidebar} />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/faq" element={<FAQ />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/geography" element={<Geography />} />
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>

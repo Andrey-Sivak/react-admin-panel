4d6273edb1e26d789eb78e429f076fa5500cd1c3
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2023-09-03 18:09:30 +0300

    try to use HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import {ColorModeContext, useMode} from "./theme";
 import Sidebar from "./sceens/global/Sidebar";
 import {CssBaseline, ThemeProvider} from "@mui/material";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Team from "./sceens/team";
 import Topbar from "./sceens/global/Topbar";
 import Dashboard from "./sceens/dashboard";
@@ -28,21 +28,19 @@ function App() {
                     <Sidebar isSidebar={isSidebar} />
                     <main className="content">
                         <Topbar setIsSidebar={setIsSidebar} />
-                        <BrowserRouter basename="/react-admin-panel">
-                            <Routes>
-                                <Route path="/" element={<Dashboard />} />
-                                <Route path="/team" element={<Team />} />
-                                <Route path="/contacts" element={<Contacts />} />
-                                <Route path="/invoices" element={<Invoices />} />
-                                <Route path="/form" element={<Form />} />
-                                <Route path="/calendar" element={<Calendar />} />
-                                <Route path="/faq" element={<FAQ />} />
-                                <Route path="/bar" element={<Bar />} />
-                                <Route path="/pie" element={<Pie />} />
-                                <Route path="/line" element={<Line />} />
-                                <Route path="/geography" element={<Geography />} />
-                            </Routes>
-                        </BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path="/faq" element={<FAQ />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/geography" element={<Geography />} />
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
